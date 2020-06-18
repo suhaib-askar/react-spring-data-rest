b6606ecb37929801901bfdbf441b1186e06fb092
Suhaib Askar <suhaibaskar@gmail.com>
2020-06-18 15:28:33 +0530

    Adding the grabbed code

---
+++ src/main/java/com/example/suhaibaskar/SuhaibaskarApplication.java
@@ -0,0 +1,13 @@
+package com.example.suhaibaskar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhaibaskarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuhaibaskarApplication.class, args);
+	}
+
+}
